c9f63e7d0441885cf1ad823ab81a58a99cf965a3
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-22 14:39:52 +0900

    1

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 import { Text, StyleSheet } from "react-native";
 
-const onPressTitle = () => {
-  console.log("title pressed");
-};
+const pressButton = () => {};
 
 const TextInANest = () => {
   const titleText = useState("지지 않는 꽃");
   const bodyText = useState("사실 이건 새둥지라고 볼수없다.");
+  const [ID, setID] = useState(0);
 
   return (
     <Text style={styles.baseText}>
-      <Text style={styles.titleText} onPress={onPressTitle}>
+      <Text style={styles.titleText} onPress={pressButton}>
         {titleText}
         {"\n"}
         {"\n"}
@@ -24,8 +23,8 @@ const TextInANest = () => {
 const styles = StyleSheet.create({
   titleText: {
     fontSize: 20,
-    fontWeight: "bold"
-  }
+    fontWeight: "bold",
+  },
 });
 
 export default TextInANest;
