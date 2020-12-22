7c55b297adf091e19e3a1cf58e2f2d75218c39dc
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-22 15:27:41 +0900

    20.12.23 15:27
    버튼 함수 분리 성공!

--- App.js
+++ App.js
@@ -1,31 +1,21 @@
 import React, { useState } from "react";
 import { Text, StyleSheet } from "react-native";
+import buttonGenerator from "./buttonGenerator";
 
 const TextInANest = () => {
   const titleText = useState("지지 않는 꽃");
   const bodyText = useState("사실 이건 새둥지라고 볼수없다.");
-  const [ID, setID] = useState(0);
-
-  const pressButton = () => {
-    setID((ID) => ID + 1);
-  };
 
   return (
     <Text style={styles.baseText}>
-      <Text>
-        {ID}쪽이네요!{"\n"}
-        {"\n"}
-      </Text>
-      <Text style={styles.titleText} onPress={pressButton}>
-        다음
-      </Text>
       <Text numberOfLines={5}>{bodyText}</Text>
+      {buttonGenerator()}
     </Text>
   );
 };
 
 const styles = StyleSheet.create({
-  titleText: {
+  baseText: {
     fontSize: 20,
     fontWeight: "bold",
   },
