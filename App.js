78f822dd194d4ccd22699f729a0d6b485a418f63
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-22 14:49:08 +0900

    basic
    useState 구현

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import { Text, StyleSheet } from "react-native";
 
-const pressButton = () => {};
-
 const TextInANest = () => {
   const titleText = useState("지지 않는 꽃");
   const bodyText = useState("사실 이건 새둥지라고 볼수없다.");
   const [ID, setID] = useState(0);
 
+  const pressButton = () => {
+    setID((ID) => ID + 1);
+  };
+
   return (
     <Text style={styles.baseText}>
+      <Text>
+        {ID}
+        {"\n"}
+        {"\n"}
+      </Text>
       <Text style={styles.titleText} onPress={pressButton}>
-        {titleText}
-        {"\n"}
-        {"\n"}
+        다음
       </Text>
       <Text numberOfLines={5}>{bodyText}</Text>
     </Text>
